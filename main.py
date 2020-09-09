79f5eedc61c80faf9ce196d4b64a63f6ff0577fd
Matt Clarke <matt.clarke@esss.se>
2020-09-09 12:47:22 +0200

    working

---
+++ main.py
@@ -0,0 +1,2 @@
+def fizz_buzz(input):
+    return 1
--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+from main import fizz_buzz
+
 def test_example_of_an_equality_test():
     assert 2 + 2 == 4
 
@@ -5,3 +7,7 @@ def test_example_of_an_equality_test():
 def test_example_of_null_test():
     import time
     assert time.monotonic_ns() is not None
+
+
+def test_fizz_buzz_of_one():
+    assert fizz_buzz(1) == 1
