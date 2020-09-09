11906ea552a5dcc5c946ff894f2d4f26a1b2180f
Matt Clarke <matt.clarke@ess.eu>
2020-09-09 12:33:31 +0000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,3 @@
-from main import fizz_buzz
-
 def test_example_of_an_equality_test():
     assert 2 + 2 == 4
 
@@ -7,31 +5,3 @@ def test_example_of_an_equality_test():
 def test_example_of_null_test():
     import time
     assert time.monotonic_ns() is not None
-
-
-def test_fizz_buzz_of_one():
-    assert fizz_buzz(1) == 1
-
-
-def test_fizz_buzz_of_three():
-    assert fizz_buzz(3) == "fizz"
-
-
-def test_fizz_buzz_of_five():
-    assert fizz_buzz(5) == "buzz"
-
-
-def test_fizz_buzz_of_two():
-    assert fizz_buzz(2) == 2
-
-
-def test_fizz_buzz_of_six():
-    assert fizz_buzz(6) == "fizz"
-
-
-def test_fizz_buzz_of_ten():
-    assert fizz_buzz(10) == "buzz"
-
-
-def test_fizz_buzz_of_fifteen():
-    assert fizz_buzz(15) == "fizzbuzz"
