49242026139a904a4591f78f53cff21ed193d3ae
Matt Clarke <matt.clarke@ess.eu>
2020-09-09 12:32:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-def fizz_buzz(input):
-    if input % 3 == 0 and input % 5 == 0:
-        return "fizzbuzz"
-    if input % 3 == 0:
-        return "fizz"
-    elif input % 5 == 0:
-        return "buzz"
-    return input
+
