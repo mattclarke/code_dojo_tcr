f03d2bc6485f775a6b099c0d85f97fe507d49b77
Matt Clarke <matt.clarke@esss.se>
2020-09-25 10:51:08 +0200

    Generate test results for golden master comparison via subprocess

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def test_example_of_an_equality_test():
     assert 2 + 2 == 4
 
@@ -5,3 +7,10 @@ def test_example_of_an_equality_test():
 def test_example_of_null_test():
     import time
     assert time.monotonic_ns() is not None
+
+
+def test_golden_master():
+    result = subprocess.check_output(["python", "main.py"]).decode()
+    with open("golden_master.txt") as f:
+        golden_master = f.read()
+    assert golden_master == result
