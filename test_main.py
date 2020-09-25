1c7fd0386f633e2890118079ba58e55be610ea5f
Matt Clarke <matt.clarke@esss.se>
2020-09-25 10:51:46 +0200

    Remove example tests

--- test_main.py
+++ test_main.py
@@ -1,13 +1,5 @@
 import subprocess
 
-def test_example_of_an_equality_test():
-    assert 2 + 2 == 4
-
-
-def test_example_of_null_test():
-    import time
-    assert time.monotonic_ns() is not None
-
 
 def test_golden_master():
     result = subprocess.check_output(["python", "main.py"]).decode()
